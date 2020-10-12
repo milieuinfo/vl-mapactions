945c882d50e77fa3ee8f28cd429612924b3b7fa1
Tom Coemans <tom.coemans@gmail.com>
2020-10-12 09:21:54 +0200

    vl prefix

--- src/vl-mapactions-all.js
+++ src/vl-mapactions-all.js
@@ -1,23 +1,23 @@
-import {CustomMap} from './vl-mapactions-custom-map';
-import {MapWithActions} from './vl-mapactions-map-with-actions';
-import {BoxSelectAction} from './vl-mapactions-box-select-action';
-import {DeleteAction} from './vl-mapactions-delete-action';
-import {DrawAction} from './vl-mapactions-draw-action';
-import {DrawLijnstukAction} from './vl-mapactions-draw-lijnstuk-action';
-import {DrawRectangleAction} from './vl-mapactions-draw-rectangle-action';
-import {HighlightAction} from './vl-mapactions-highlight-action';
-import {MapAction} from './vl-mapactions-mapaction';
-import {MeasureAction} from './vl-mapactions-measure-action';
-import {ModifyAction} from './vl-mapactions-modify-action';
-import {ModifyAndTranslateAction} from './vl-mapactions-modify-and-translate-action';
-import {SelectAction} from './vl-mapactions-select-action';
-import {SelectActions} from './vl-mapactions-select-actions';
-import {ShowInfoAction} from './vl-mapactions-show-info-action';
-import {ShowInfoSelectAction} from './vl-mapactions-show-info-select-action';
-import {SplitAction} from './vl-mapactions-split-action';
-import {Tooltips} from './vl-mapactions-tooltips';
-import {TranslateAction} from './vl-mapactions-translate-action';
-import {SnapInteraction} from './vl-mapactions-snap-interaction';
+import {VlMap} from './vl-mapactions-custom-map';
+import {VlMapWithActions} from './vl-mapactions-map-with-actions';
+import {VlBoxSelectAction} from './vl-mapactions-box-select-action';
+import {VlDeleteAction} from './vl-mapactions-delete-action';
+import {VlDrawAction} from './vl-mapactions-draw-action';
+import {VlDrawLijnstukAction} from './vl-mapactions-draw-lijnstuk-action';
+import {VlDrawRectangleAction} from './vl-mapactions-draw-rectangle-action';
+import {VlHighlightAction} from './vl-mapactions-highlight-action';
+import {VlMapAction} from './vl-mapactions-mapaction';
+import {VlMeasureAction} from './vl-mapactions-measure-action';
+import {VlModifyAction} from './vl-mapactions-modify-action';
+import {VlModifyAndTranslateAction} from './vl-mapactions-modify-and-translate-action';
+import {VlSelectAction} from './vl-mapactions-select-action';
+import {VlSelectActions} from './vl-mapactions-select-actions';
+import {VlShowInfoAction} from './vl-mapactions-show-info-action';
+import {VlShowInfoSelectAction} from './vl-mapactions-show-info-select-action';
+import {VlSplitAction} from './vl-mapactions-split-action';
+import {VlTooltips} from './vl-mapactions-tooltips';
+import {VlTranslateAction} from './vl-mapactions-translate-action';
+import {VlSnapInteraction} from './vl-mapactions-snap-interaction';
 import OlLayerGroup from 'ol/layer/Group';
 import OlVectorLayer from 'ol/layer/Vector';
 import OlTileLayer from 'ol/layer/Tile';
@@ -38,26 +38,26 @@ import * as OlExtent from 'ol/extent';
 import * as OlLoadingstrategy from 'ol/loadingstrategy';
 
 export {
-  CustomMap,
-  MapWithActions,
-  SnapInteraction,
-  BoxSelectAction,
-  DeleteAction,
-  DrawAction,
-  DrawLijnstukAction,
-  DrawRectangleAction,
-  HighlightAction,
-  MapAction,
-  MeasureAction,
-  ModifyAction,
-  ModifyAndTranslateAction,
-  SelectAction,
-  SelectActions,
-  ShowInfoAction,
-  ShowInfoSelectAction,
-  SplitAction,
-  Tooltips,
-  TranslateAction,
+  VlMap,
+  VlMapWithActions,
+  VlSnapInteraction,
+  VlBoxSelectAction,
+  VlDeleteAction,
+  VlDrawAction,
+  VlDrawLijnstukAction,
+  VlDrawRectangleAction,
+  VlHighlightAction,
+  VlMapAction,
+  VlMeasureAction,
+  VlModifyAction,
+  VlModifyAndTranslateAction,
+  VlSelectAction,
+  VlSelectActions,
+  VlShowInfoAction,
+  VlShowInfoSelectAction,
+  VlSplitAction,
+  VlTooltips,
+  VlTranslateAction,
   OlLayerGroup,
   OlVectorLayer,
   OlTileLayer,
