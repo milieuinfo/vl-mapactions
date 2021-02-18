03d947d66a0f9881da2f6210f4bb7046e6cbf5df
Tom Coemans <tom.coemans@gmail.com>
2021-02-18 11:35:26 +0100

    ESLint fix

--- src/vl-mapactions-box-select-action.js
+++ src/vl-mapactions-box-select-action.js
@@ -1,5 +1,5 @@
 import DragBox from 'ol/interaction/DragBox';
-import {Fill, Stroke, Style, Circle} from 'ol/style';
+import {Stroke, Style} from 'ol/style';
 import {VlSelectAction} from './vl-mapactions-select-action';
 
 export class VlBoxSelectAction extends VlSelectAction {
