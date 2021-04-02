33752ccbe639cac5957ad7b05e6c391ad68f95dd
Tom Coemans <tom.coemans@gmail.com>
2021-04-02 11:00:32 +0200

    trivial code refactor

--- src/vl-mapactions-snap-interaction.js
+++ src/vl-mapactions-snap-interaction.js
@@ -1,14 +1,10 @@
 import Snap from 'ol/interaction/Snap';
 
 export class VlSnapInteraction extends Snap {
-  constructor(source, options) {
+  constructor(source, options = {}) {
     const snapOptions = Object.assign({}, options);
     snapOptions.source = source;
-    if (options && options.pixelTolerance) {
-      snapOptions.pixelTolerance = options.pixelTolerance;
-    } else {
-      snapOptions.pixelTolerance = 7;
-    }
+    snapOptions.pixelTolerance = options.pixelTolerance || 7;
     super(snapOptions);
   }
 }
